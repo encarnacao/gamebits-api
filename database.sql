a049a6784ef7e76bd3b2fb5e1a71bc67eef0c608
encarnacao <cencarnacaoq@gmail.com>
2023-04-09 12:21:19 -0300

    fix: change numeric on review rating

--- database.sql
+++ database.sql
@@ -1,55 +1,60 @@
-CREATE TABLE "users" (
-    "id" serial NOT NULL,
-    "name" varchar(50) NOT NULL,
-    "picture_url" TEXT,
-    "email" varchar(60) NOT NULL UNIQUE,
-    "password" varchar(60) NOT NULL,
-    "created_at" TIMESTAMP NOT NULL DEFAULT NOW(),
-    CONSTRAINT "users_pk" PRIMARY KEY ("id")
-);
-
-CREATE TABLE "reviews" (
-    "id" serial NOT NULL,
-    "user_id" int NOT NULL,
-    "game_id" int NOT NULL,
-    rating NUMERIC(2,1) NOT NULL,
-    "review" TEXT NOT NULL,
-    "created_at" TIMESTAMP NOT NULL DEFAULT NOW(),
-    CONSTRAINT "reviews_pk" PRIMARY KEY ("id"),
-    CONSTRAINT rating_range CHECK (rating >= 0 AND rating <= 10 AND MOD(rating, 0.5) = 0)
-);
-
-CREATE TABLE "games" (
-    "id" serial NOT NULL,
-    "name" TEXT NOT NULL UNIQUE,
-    CONSTRAINT "games_pk" PRIMARY KEY ("id")
-);
-
+-- CreateTable
 CREATE TABLE "comments" (
-    "id" serial NOT NULL,
-    "review_id" int NOT NULL,
-    "user_id" int NOT NULL,
-    "text" varchar(250) NOT NULL,
-    "created_at" TIMESTAMP NOT NULL DEFAULT NOW(),
+    "id" SERIAL NOT NULL,
+    "review_id" INTEGER NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "text" VARCHAR(250) NOT NULL,
+    "created_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
     CONSTRAINT "comments_pk" PRIMARY KEY ("id")
 );
 
-ALTER TABLE
-    "reviews"
-ADD
-    CONSTRAINT "reviews_fk0" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE;
+-- CreateTable
+CREATE TABLE "games" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
 
-ALTER TABLE
-    "reviews"
-ADD
-    CONSTRAINT "reviews_fk1" FOREIGN KEY ("game_id") REFERENCES "games"("id") ON DELETE CASCADE;
+    CONSTRAINT "games_pk" PRIMARY KEY ("id")
+);
 
-ALTER TABLE
-    "comments"
-ADD
-    CONSTRAINT "comments_fk0" FOREIGN KEY ("review_id") REFERENCES "reviews"("id") ON DELETE CASCADE;
+-- CreateTable
+CREATE TABLE "reviews" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "game_id" INTEGER NOT NULL,
+    "rating" DECIMAL(3,1) NOT NULL,
+    "review" TEXT NOT NULL,
+    "created_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-ALTER TABLE
-    "comments"
-ADD
-    CONSTRAINT "comments_fk1" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE;
+    CONSTRAINT "reviews_pk" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(50) NOT NULL,
+    "picture_url" TEXT,
+    "email" VARCHAR(60) NOT NULL,
+    "password" VARCHAR(60) NOT NULL,
+    "created_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pk" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "unique_name" ON "games"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "comments" ADD CONSTRAINT "comments_fk0" FOREIGN KEY ("review_id") REFERENCES "reviews"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "comments" ADD CONSTRAINT "comments_fk1" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "reviews" ADD CONSTRAINT "reviews_fk0" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "reviews" ADD CONSTRAINT "reviews_fk1" FOREIGN KEY ("game_id") REFERENCES "games"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
