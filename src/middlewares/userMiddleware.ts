9e680fbbd6e022e6624c35a0595ee648af3c2034
encarnacao <cencarnacaoq@gmail.com>
2023-04-09 02:18:29 -0300

    feat: add conflict check

---
+++ src/middlewares/userMiddleware.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from "express";
+import errors from "../errors/index.js";
+import userRepository from "../repositories/userRepository.js";
+
+async function checkConflict(req: Request, res: Response, next: NextFunction) {
+	const { email } = req.body;
+	try {
+		const user = await userRepository.findUserByEmail(email);
+		if (user.id) {
+			throw errors.emailConflictError(email);
+		}
+		next();
+	} catch (err) {
+		next(err);
+	}
+}
+
+export default { checkConflict };
--- src/routers/userRouters.ts
+++ src/routers/userRouters.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { validateSchema } from "../middlewares/validateSchema.js";
 import userController from "../controllers/userController.js";
 import { userSchema } from "../schemas/userSchema.js";
+import userMiddleware from "../middlewares/userMiddleware.js";
 
 const userRouter = Router();
 userRouter.post(
 	"/signup",
 	validateSchema(userSchema),
+	userMiddleware.checkConflict,
 	userController.createUser
 );
 
