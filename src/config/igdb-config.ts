bfe771dfe6cb060efbf640fc4e5becced3496ede
encarnacao <cencarnacaoq@gmail.com>
2023-06-10 18:54:42 -0300

    refactor: export config instead of headers

--- src/config/igdb-config.ts
+++ src/config/igdb-config.ts
@@ -1,7 +1,9 @@
 import { loadEnv } from "./env";
 
 loadEnv();
-export const headers = {
-  "client-id": process.env.IGDB_CLIENT_ID,
-  authorization: `Bearer ${process.env.IGDB_TOKEN}`,
+export const config = {
+  headers: {
+    "client-id": process.env.IGDB_CLIENT_ID,
+    authorization: `Bearer ${process.env.IGDB_TOKEN}`,
+  },
 };
