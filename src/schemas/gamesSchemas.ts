61d793b8c02e6e44b9b0ffa9ac39a513ecb5fa30
encarnacao <cencarnacaoq@gmail.com>
2023-06-11 11:12:14 -0300

    feat: add schemas for params

--- src/schemas/gamesSchemas.ts
+++ src/schemas/gamesSchemas.ts
@@ -4,6 +4,14 @@ export const searchSchema = Joi.object<searchQuery>({
   name: Joi.string().required(),
 });
 
+export const gameParamsSchema = Joi.object<gameParams>({
+  id: Joi.number().required(),
+});
+
+export interface gameParams {
+  id: number;
+}
+
 export interface searchQuery {
   name: string;
 }
