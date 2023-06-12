eff7be2b1d619e7d643fbb20d01e8fb5f5d8da05
encarnacao <cencarnacaoq@gmail.com>
2023-06-11 23:15:48 -0300

    feat: create repository for library

---
+++ src/repositories/libraryRepository/index.ts
@@ -0,0 +1,46 @@
+import { Prisma } from "@prisma/client";
+import { prisma } from "@/config";
+
+async function addGameToLibrary(
+  game_id: number,
+  user_id: number,
+  wishlist: boolean
+) {
+  return await prisma.libraries.create({
+    data: { game_id, user_id, wishlist },
+  });
+}
+
+async function removeGameFromLibrary(id: number) {
+  return await prisma.libraries.delete({
+    where: { id },
+  });
+}
+
+async function updateLibraryEntry(id: number, props: Prisma.librariesUpdateInput) {
+  return await prisma.libraries.update({
+    where: { id },
+    data: props,
+  });
+}
+
+async function searchLibraryEntry(user_id: number, game_id: number) {
+  return await prisma.libraries.findFirst({
+    where: { user_id, game_id },
+  });
+}
+
+async function searchLibrary(user_id: number, wishlist: boolean) {
+  return await prisma.libraries.findMany({
+    where: { user_id, wishlist },
+  });
+}
+
+
+export default {
+  addGameToLibrary,
+  removeGameFromLibrary,
+  updateLibraryEntry,
+  searchLibraryEntry,
+  searchLibrary,
+};
