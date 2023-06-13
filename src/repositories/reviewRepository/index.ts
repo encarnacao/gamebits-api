197df1c2b7a7a4b211cba46e65fdf51dd6ee2b53
encarnacao <cencarnacaoq@gmail.com>
2023-06-13 09:44:47 -0300

    fix: return relevant information on query

--- src/repositories/reviewRepository/index.ts
+++ src/repositories/reviewRepository/index.ts
@@ -1,34 +1,51 @@
 import { prisma } from "@/config";
 
-async function createReview(user_id: number, game_id: number, text: string, rating: number){
+async function createReview(
+  user_id: number,
+  game_id: number,
+  text: string,
+  rating: number
+) {
   return await prisma.reviews.create({
     data: { user_id, game_id, text, rating },
   });
 }
 
-async function deleteReview(id: number){
+async function deleteReview(id: number) {
   return await prisma.reviews.delete({
     where: { id },
   });
 }
 
-async function searchReview(id: number){
+async function searchReview(id: number) {
   return await prisma.reviews.findFirst({
     where: { id },
   });
 }
 
-async function searchReviews(game_id: number){
+async function searchReviews(game_id: number) {
   return await prisma.reviews.findMany({
     where: { game_id },
-    include: { users: true, votes: true },
+    include: {
+      users: {
+        select: { id: true, username: true, image_url: true },
+      },
+      votes: {
+        select: { user_id: true, up_vote: true },
+      },
+    },
   });
 }
 
-async function searchUserReviews(user_id: number){
+async function searchUserReviews(user_id: number) {
   return await prisma.reviews.findMany({
     where: { user_id },
-    include: { games: true, votes: true },
+    include: {
+      games: true,
+      votes: {
+        select: { user_id: true, up_vote: true },
+      },
+    },
   });
 }
 
@@ -38,4 +55,4 @@ export default {
   searchReview,
   searchReviews,
   searchUserReviews,
-}
+};
