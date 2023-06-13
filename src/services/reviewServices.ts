9755ce2480988e8850e84e7d732e7719e5097e94
encarnacao <cencarnacaoq@gmail.com>
2023-06-12 21:15:10 -0300

    feat: add create review

---
+++ src/services/reviewServices.ts
@@ -0,0 +1,37 @@
+import errors from "@/errors";
+import { ReviewBody } from "@/protocols";
+import reviewRepository from "@/repositories/reviewRepository";
+
+async function validateUserReview(reviewId: number, userId: number) {
+  const review = await reviewRepository.searchReview(reviewId);
+  if (!review) {
+    throw errors.notFoundError();
+  }
+  if (review.user_id !== userId) {
+    throw errors.forbiddenError();
+  }
+  return review;
+}
+
+async function validateReview(userId: number, gameId: number) {
+  const search = await reviewRepository.searchUserReviews(userId);
+  const review = search.find((review) => review.game_id === gameId);
+  if (review) {
+    throw errors.conflictError();
+  }
+}
+
+async function createReview(userId: number, body: ReviewBody) {
+  await validateReview(userId, body.game_id);
+  const review = await reviewRepository.createReview(
+    userId,
+    body.game_id,
+    body.text,
+    body.rating
+  );
+  return review;
+}
+
+const reviewServices = {
+  createReview,
+};
