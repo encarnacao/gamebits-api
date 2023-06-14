39dcda325fe35f1f2020d169bba2c35c5a628f8c
encarnacao <cencarnacaoq@gmail.com>
2023-06-14 12:13:35 -0300

    feat: add users router

--- src/routers/usersRouter.ts
+++ src/routers/usersRouter.ts
@@ -1,18 +1,19 @@
 import { Router } from "express";
-import { validateBody } from "../middlewares/validateSchema.js";
+import { validateBody, validateParams } from "../middlewares/validateSchema.js";
 import { userSchema, userSignInSchema } from "../schemas/userSchema.js";
 import userMiddleware from "../middlewares/userMiddleware.js";
-import { createUser, signIn } from "@/controllers/userController.js";
+import { createUser, findUser, signIn } from "@/controllers/userController.js";
+import { paramsSchema } from "@/schemas/genericSchemas.js";
 
 const usersRouter = Router();
-usersRouter.post(
-  "/signup",
-  validateBody(userSchema),
-  userMiddleware.checkConflict,
-  createUser
-);
-
-usersRouter.post("/signin", validateBody(userSignInSchema), signIn);
-
+usersRouter
+  .post(
+    "/signup",
+    validateBody(userSchema),
+    userMiddleware.checkConflict,
+    createUser
+  )
+  .post("/signin", validateBody(userSignInSchema), signIn)
+  .get("/:id", validateParams(paramsSchema), findUser);
 
 export default usersRouter;
