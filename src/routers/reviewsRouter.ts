7c164d3fd28893bb00401a26606183f7081311e3
encarnacao <cencarnacaoq@gmail.com>
2023-06-14 13:23:41 -0300

    feat: add get reviews routes for user and game

--- src/routers/reviewsRouter.ts
+++ src/routers/reviewsRouter.ts
@@ -3,22 +3,25 @@ import { Router } from "express";
 import validateCredentials from "../middlewares/authMiddleware.js";
 import { validateBody, validateParams } from "../middlewares/validateSchema.js";
 import { reviewSchema } from "../schemas/reviewSchema.js";
-import { createReview, deleteReview } from "@/controllers/reviewController.js";
+import {
+  createReview,
+  deleteReview,
+  getGameReviews,
+  getUserReviews,
+} from "@/controllers/reviewController.js";
 import { paramsSchema } from "@/schemas/genericSchemas.js";
 
 const reviewsRouter = Router();
 
-reviewsRouter.post(
-	"/",
-	validateCredentials,
-	validateBody(reviewSchema),
-	createReview
-)
-.delete(
-	"/:id",
-	validateCredentials,
-	validateParams(paramsSchema),
-	deleteReview
-)
+reviewsRouter
+  .post("/", validateCredentials, validateBody(reviewSchema), createReview)
+  .delete(
+    "/:id",
+    validateCredentials,
+    validateParams(paramsSchema),
+    deleteReview
+  )
+  .get("/:id", validateParams(paramsSchema), getGameReviews)
+  .get("/user/:id", validateParams(paramsSchema), getUserReviews);
 
 export default reviewsRouter;
