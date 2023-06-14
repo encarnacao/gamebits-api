39c77940037079b9017d8f1b5dbcbafe38c5cff6
encarnacao <cencarnacaoq@gmail.com>
2023-06-13 23:30:56 -0300

    feat: add get and delete controllers

--- src/controllers/reviewController.ts
+++ src/controllers/reviewController.ts
@@ -1,22 +1,39 @@
 import { Request, Response, NextFunction } from "express";
-import { ReviewBody } from "@/protocols";
+import { ReviewBody, idParams } from "@/protocols";
+import reviewServices from "@/services/reviewServices";
+import { users } from "@prisma/client";
 
 async function createReview(req: Request, res: Response, next: NextFunction) {
-	const body = req.body as ReviewBody;
-	const userId = Number(res.locals.user.id);
-	try {
-	} catch (err) {
-		next(err);
-	}
+  const body = req.body as ReviewBody;
+  const user: users = res.locals.user;
+  try {
+    const review = await reviewServices.createReview(user.id, body);
+    res.status(201).send(review);
+  } catch (err) {
+    next(err);
+  }
 }
 
-async function getAll(req: Request, res: Response, next: NextFunction) {
-	const queryParams = req.query;
-	try {
-
-	} catch (err) {
-		next(err);
-	}
+async function getGameReview(req: Request, res: Response, next: NextFunction) {
+  const { id } = req.params as unknown as idParams;
+  try {
+    const reviews = await reviewServices.getReviews(Number(id));
+    res.send(reviews);
+  } catch (err) {
+    next(err);
+  }
 }
 
-export default { createReview, getAll };
+async function deleteReview(req: Request, res: Response, next: NextFunction) {
+  const { id } = req.params as unknown as idParams;
+  const user: users = res.locals.user;
+  try {
+    await reviewServices.deleteReview(Number(id), user.id);
+    res.sendStatus(204);
+  } catch (err) {
+    next(err);
+  }
+}
+
+
+export default { createReview, getGameReview, deleteReview };
