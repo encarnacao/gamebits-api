4b4c04b16ccfb4fb160eb6777742432dbff81d79
encarnacao <cencarnacaoq@gmail.com>
2023-06-13 23:30:38 -0300

    feat: add search user by id

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,13 +2,13 @@ import "dotenv/config";
 import { Request, Response, NextFunction } from "express";
 import userServices from "../services/userServices.js";
 import jwt from "jsonwebtoken";
-import errors from "@/errors";
-import { SignInBody } from "@/protocols.js";
+import { SignInBody, UserParams, idParams } from "@/protocols.js";
 
 
 async function createUser(req: Request, res: Response, next: NextFunction) {
+	const userData = req.body as UserParams;
 	try {
-		const create = await userServices.createUser(req.body);
+		const create = await userServices.createUser(userData);
 		delete create.password;
 		res.send(create);
 	} catch (err) {
@@ -22,6 +22,16 @@ async function signIn(req: Request, res: Response, next: NextFunction) {
 		const user = await userServices.getUserByEmail(userData);
 		const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET);
 		res.send({ token });
+	} catch (err) {
+		next(err);
+	}
+}
+
+async function findUser(req: Request, res: Response, next: NextFunction) {
+	const params = req.params as unknown as idParams;
+	try {
+		const user = await userServices.getUserById(Number(params.id));
+		res.send(user);
 	} catch (err) {
 		next(err);
 	}
