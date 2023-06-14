c677b7edbd4062a539b9769fa04ef21e11672cf6
encarnacao <cencarnacaoq@gmail.com>
2023-06-14 13:20:43 -0300

    feat: add delete review route

--- src/routers/reviewsRouter.ts
+++ src/routers/reviewsRouter.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 
 import validateCredentials from "../middlewares/authMiddleware.js";
-import { validateBody } from "../middlewares/validateSchema.js";
+import { validateBody, validateParams } from "../middlewares/validateSchema.js";
 import { reviewSchema } from "../schemas/reviewSchema.js";
-import { createReview } from "@/controllers/reviewController.js";
+import { createReview, deleteReview } from "@/controllers/reviewController.js";
+import { paramsSchema } from "@/schemas/genericSchemas.js";
 
 const reviewsRouter = Router();
 
@@ -12,6 +13,12 @@ reviewsRouter.post(
 	validateCredentials,
 	validateBody(reviewSchema),
 	createReview
-);
+)
+.delete(
+	"/:id",
+	validateCredentials,
+	validateParams(paramsSchema),
+	deleteReview
+)
 
 export default reviewsRouter;
