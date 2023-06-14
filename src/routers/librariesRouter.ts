0238427be234fe84bc633e1c2ef41688d769f854
encarnacao <cencarnacaoq@gmail.com>
2023-06-14 13:15:59 -0300

    feat: add libraries router

--- src/routers/librariesRouter.ts
+++ src/routers/librariesRouter.ts
@@ -1,16 +1,36 @@
 import { Router } from "express";
-import { validateParams } from "../middlewares/validateSchema.js";
+import { validateBody, validateParams } from "../middlewares/validateSchema.js";
 import validateCredentials from "@/middlewares/authMiddleware.js";
-import { librarySchema } from "@/schemas/librarySchemas.js";
-import { addGame } from "@/controllers/libraryController.js";
+import { libraryUpdateSchema } from "@/schemas/librarySchemas.js";
+import {
+  addGame,
+  removeGame,
+  updateEntry,
+} from "@/controllers/libraryController.js";
+import { paramsSchema } from "@/schemas/genericSchemas.js";
 
 const librariesRouter = Router();
 
-librariesRouter.post(
-  "/add/:id",
-  validateCredentials,
-  validateParams(librarySchema),
-  addGame
-);
+librariesRouter
+  .post(
+    "/add/:id",
+    validateCredentials,
+    validateParams(paramsSchema),
+    addGame(false)
+  )
+  .post(
+    "/wishlist/:id",
+    validateCredentials,
+    validateParams(paramsSchema),
+    addGame(true)
+  )
+  .delete("/:id", validateCredentials, validateParams(paramsSchema), removeGame)
+  .put(
+    "/:id",
+    validateCredentials,
+    validateParams(paramsSchema),
+    validateBody(libraryUpdateSchema),
+    updateEntry
+  );
 
 export default librariesRouter;
--- src/schemas/librarySchemas.ts
+++ src/schemas/librarySchemas.ts
@@ -1,11 +1,9 @@
-import { LibraryUpdate, idParams } from "@/protocols";
+import { LibraryUpdate } from "@/protocols";
 import Joi from "joi";
 
-export const librarySchema = Joi.object<idParams>({
-  id: Joi.number().required(),
-});
-
 export const libraryUpdateSchema = Joi.object<LibraryUpdate>({
-  status: Joi.string().valid("finished", "platinum", "completion_time").required(),
+  status: Joi.string()
+    .valid("finished", "platinum", "completion_time")
+    .required(),
   completion_time: Joi.number().min(0).precision(1),
 });
