f817271b2d52dddd4111a8d55c3904f3eee42e69
encarnacao <cencarnacaoq@gmail.com>
2023-06-14 13:31:58 -0300

    feat: add vote Router

--- src/routers/votesRouter.ts
+++ src/routers/votesRouter.ts
@@ -1,5 +1,19 @@
+import {
+  createVote,
+  deleteVote,
+  updateVote,
+} from "@/controllers/voteController";
+import validateCredentials from "@/middlewares/authMiddleware";
+import { validateBody, validateParams } from "@/middlewares/validateSchema";
+import { paramsSchema } from "@/schemas/genericSchemas";
+import { votingSchema } from "@/schemas/voteSchemas";
 import { Router } from "express";
 
 const votesRouter = Router();
+
+votesRouter
+  .post("/", validateCredentials, validateBody(votingSchema), createVote)
+  .delete("/:id", validateCredentials, validateParams(paramsSchema), deleteVote)
+  .put("/:id", validateCredentials, validateParams(paramsSchema), updateVote);
 
 export default votesRouter;
