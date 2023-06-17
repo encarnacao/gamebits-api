c77f2177c6152d876c0553f412ac91c55115a833
encarnacao <cencarnacaoq@gmail.com>
2023-06-17 16:04:14 -0300

    feat: add custom validation so username can't be just spaces

--- src/schemas/userSchemas.ts
+++ src/schemas/userSchemas.ts
@@ -2,7 +2,12 @@ import { UserParams } from "@/protocols";
 import Joi from "joi";
 
 export const userSchema = Joi.object<UserParams>({
-	username: Joi.string().required().min(3),
+	username: Joi.string().required().min(3).custom((value, helpers) => {
+    if (value.trim().length === 0) {
+      return helpers.error('any.invalid');
+    }
+    return value;
+  }),
 	email: Joi.string().email().required(),
 	password: Joi.string().min(6).required(),
 
