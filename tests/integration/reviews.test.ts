ef35c0d6681846a4a494b0c9342ccce5e3b56393
encarnacao <cencarnacaoq@gmail.com>
2023-06-17 19:01:21 -0300

    test: add tests for delete review

--- tests/integration/reviews.test.ts
+++ tests/integration/reviews.test.ts
@@ -61,7 +61,6 @@ describe("POST /reviews", () => {
         .post("/reviews")
         .set("Authorization", `Bearer ${token}`)
         .send(body);
-      console.log(response.error);
       expect(response.status).toBe(httpStatus.CREATED);
     });
     it("should return status 409 if user already reviewed the game", async () => {
@@ -75,6 +74,59 @@ describe("POST /reviews", () => {
         .set("Authorization", `Bearer ${token}`)
         .send(body);
       expect(response.status).toBe(httpStatus.CONFLICT);
+    });
+  });
+});
+
+describe("DELETE /reviews/:id", () => {
+  it("should return status 401 if no token is provided", async () => {
+    const response = await server.delete("/reviews/1");
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+  it("should return status 401 if token is invalid", async () => {
+    const token = faker.lorem.word();
+    const response = await server
+      .delete("/reviews/1")
+      .set("Authorization", token);
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+  describe("when token is valid", () => {
+    it("should return status 422 if params is invalid", async () => {
+      const user = await createUser();
+      const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET);
+      const response = await server
+        .delete("/reviews/invalid")
+        .set("Authorization", `Bearer ${token}`);
+      expect(response.status).toBe(httpStatus.UNPROCESSABLE_ENTITY);
+    });
+    it("should return status 404 if review does not exist", async () => {
+      const user = await createUser();
+      const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET);
+      const response = await server
+        .delete("/reviews/1")
+        .set("Authorization", `Bearer ${token}`);
+      expect(response.status).toBe(httpStatus.NOT_FOUND);
+    });
+    it("should return status 403 if user is not the review owner", async () => {
+      const user = await createUser();
+      const otherUser = await createUser();
+      const game = await createGame();
+      const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET);
+      const review = await createReview(otherUser.id, game.id);
+      const response = await server
+        .delete(`/reviews/${review.id}`)
+        .set("Authorization", `Bearer ${token}`);
+      expect(response.status).toBe(httpStatus.FORBIDDEN);
+    });
+    it("should return status 204 if user is the review owner", async () => {
+      const user = await createUser();
+      const game = await createGame();
+      const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET);
+      const review = await createReview(user.id, game.id);
+      const response = await server
+        .delete(`/reviews/${review.id}`)
+        .set("Authorization", `Bearer ${token}`);
+      expect(response.status).toBe(httpStatus.NO_CONTENT);
     });
   });
 });
