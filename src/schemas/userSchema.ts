43400b427abac68b41aa093dca745d6db6d03858
encarnacao <cencarnacaoq@gmail.com>
2023-06-16 22:00:55 -0300

    feat: add minimun of 6 characters for password

--- src/schemas/userSchema.ts
+++ src/schemas/userSchema.ts
@@ -4,7 +4,7 @@ import Joi from "joi";
 export const userSchema = Joi.object<UserParams>({
 	username: Joi.string().required(),
 	email: Joi.string().email().required(),
-	password: Joi.string().required(),
+	password: Joi.string().min(6).required(),
 
 });
 
