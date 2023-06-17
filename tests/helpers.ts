be8f210f82706ee2e475f318ab1bbce2ec6e793b
encarnacao <cencarnacaoq@gmail.com>
2023-06-16 21:03:12 -0300

    feat: add clean db helper

---
+++ tests/helpers.ts
@@ -0,0 +1,10 @@
+import { prisma } from "./config";
+
+export async function cleanDatabase() {
+  await prisma.follows.deleteMany({});
+  await prisma.libraries.deleteMany({});
+  await prisma.games.deleteMany({});
+  await prisma.votes.deleteMany({});
+  await prisma.reviews.deleteMany({});
+  await prisma.users.deleteMany({});
+}
