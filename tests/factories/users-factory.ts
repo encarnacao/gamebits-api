ea2cbddb8fc664650a1bba8a02e38061e41f4b96
encarnacao <cencarnacaoq@gmail.com>
2023-06-16 21:23:04 -0300

    fix: use await

--- tests/factories/users-factory.ts
+++ tests/factories/users-factory.ts
@@ -6,7 +6,7 @@ import { faker } from "@faker-js/faker";
 export async function createUser(params: Partial<users> = {}) {
   const incomingPassword = params.password || faker.internet.password(6);
   const hashedPassword = await bcrypt.hash(incomingPassword, 10);
-  prisma.users.create({
+  return await prisma.users.create({
     data: {
       email: params.email || faker.internet.email(),
       username: params.username || faker.internet.userName(),
